d89b7078224c04b571d5653aafa72c4d9d4ab609
savaliyarohit <savaliyarohit333@gmail.com>
2022-12-21 17:44:23 +0000

    evenodd

---
+++ Even.java
@@ -0,0 +1,16 @@
+package evenodd;
+
+public class Even {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 25;
+        if (20 % 2 == 0){
+
+            System.out.println(20 + "number is even number");
+    }else {
+            System.out.println(25 + "number is odd number");
+
+
+        }
+    }
+}
---
+++ odd.java
@@ -0,0 +1,15 @@
+package evenodd;
+
+public class odd {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 15;
+        if (15 % 2 == 0){
+
+            System.out.println(50 + "number is even number");
+        }else {
+            System.out.println(15 + "number is odd number");
+
+        }
+    }
+}
